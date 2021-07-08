fcc99f83be41a612e4fb44ae8725f22a399b3444
André Schulz <andremichelschulz@hotmail.com>
2021-07-08 11:21:26 -0300

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-HORAINICIAL=$(date +%T)
-LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
-echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`" &>> $LOG
+#HORAINICIAL=$(date +%T)
+#LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
+#echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`" &>> $LOG
 add-apt-repository universe &>> $LOG
 add-apt-repository multiverse &>> $LOG
 sudo rm /var/cache/debconf/config.dat
@@ -17,10 +17,10 @@ sed -i '225 i ServerTokens ProductOnly' /etc/apache2/apache2.conf &>> $LOG
 sed -i '226 i ServerSignature Off' /etc/apache2/apache2.conf &>> $LOG
 systemctl restart apache2 &>> $LOG
 netstat -an | grep '80'
-HORAFINAL=`date +%T`
-HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
-HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
-TEMPO=`date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S"`
-echo -e "Tempo gasto para execução do script $0: $TEMPO"
-echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`" &>> $LOG
+#HORAFINAL=`date +%T`
+#HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
+#HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
+#TEMPO=`date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S"`
+#echo -e "Tempo gasto para execução do script $0: $TEMPO"
+#echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`" &>> $LOG
 exit 1
